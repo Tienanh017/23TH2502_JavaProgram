49f4361fc0f8d13f2fa2c0ab770c0567a692ea41
Tienanh017 <91964030+Tienanh017@users.noreply.github.com>
2024-09-23 19:29:32 +0700

    Up bai MyClass

---
+++ BaiToanVD/src/MyClass.java
@@ -0,0 +1,15 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 7;
+		int c = a +b;
+		
+		System.out.println("Học java ");
+		System.out.println("Tổng là " + c );
+		
+	}
+
+}
